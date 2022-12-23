666ac167fefc3ff73247948071760a0be39a08c6
Mohamed Alganzory <md.alganzory@gmail.com>
2022-12-24 02:01:08 +0800

    implement click status bar to view/edit note

--- extension.js
+++ extension.js
@@ -13,12 +13,13 @@ const VSCODE_DIR = path.join(
 
 const NOTES_FILE = path.join(VSCODE_DIR, "notes.json");
 
-function updateStatusBar(note) {
-	if (note === "") {
-		vscode.window.setStatusBarMessage(`**TabNote:**`);
-		return;
-	}
-	vscode.window.setStatusBarMessage(`**TabNote:** ${note}`);
+const statusBarItem = vscode.window.createStatusBarItem();
+
+function updateStatusBar(note = "") {
+	const displayNote = note.length > 10 ? note.substring(0, 10) + "..." : note;
+	statusBarItem.command = "extension.showNote";
+	statusBarItem.text = `**TabNote:** ${displayNote}`;
+	statusBarItem.show();
 }
 
 // This function is called when the extension is activated
@@ -29,7 +30,7 @@ function activate(context) {
 		fs.mkdirSync(VSCODE_DIR);
 	}
 	// upon starting VS Code, check if there is a note for the active tab, and display it in the status bar
-	if (fs.existsSync(NOTES_FILE)) {
+	if (fs.existsSync(NOTES_FILE) && vscode.window.activeTextEditor) {
 		let notes = JSON.parse(fs.readFileSync(NOTES_FILE).toString());
 		if (vscode.window.activeTextEditor.document.fileName in notes) {
 			updateStatusBar(
@@ -132,6 +133,57 @@ function activate(context) {
 		})
 	);
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand("extension.showNote", function () {
+			// Check if there is an active text editor
+			if (!vscode.window.activeTextEditor) {
+				vscode.window.showErrorMessage(
+					"Cannot show note. No active text editor found."
+				);
+				return;
+			}
+
+			// read the notes from the notes.json file
+			let notes = {};
+			if (fs.existsSync(NOTES_FILE)) {
+				notes = JSON.parse(fs.readFileSync(NOTES_FILE).toString());
+			}
+
+			let existingNote =
+				notes[vscode.window.activeTextEditor.document.fileName];
+			// Show the full note in an input box
+			vscode.window
+				.showInputBox({
+					value: existingNote || "",
+					// move the cursor to the end of the note
+					valueSelection: [
+						existingNote ? existingNote.length : 0,
+						existingNote ? existingNote.length : 0,
+					],
+					placeHolder: "Enter a note for this tab",
+					prompt: "View/Edit note for this tab",
+				})
+				.then(function (newNote) {
+					// If the user clicked "Cancel" or entered no new note, do nothing
+					if (newNote === undefined || newNote === existingNote) {
+						return;
+					}
+					// update the notes object with the new note
+					notes[vscode.window.activeTextEditor.document.fileName] =
+						newNote;
+					// write the updated notes object to the notes.json file
+					fs.writeFileSync(NOTES_FILE, JSON.stringify(notes));
+					// Update the status bar with the note if the tab corresponds to the active tab
+					if (
+						vscode.window.activeTextEditor.document.fileName in
+						notes
+					) {
+						updateStatusBar(newNote);
+					}
+				});
+		})
+	);
+
 	// upon opening a file, check if there is a note for it, and display it in the status bar
 	vscode.window.onDidChangeActiveTextEditor(function (editor) {
 		console.log("Active editor changed");
@@ -141,7 +193,7 @@ function activate(context) {
 			if (editor.document.fileName in notes) {
 				updateStatusBar(notes[editor.document.fileName]);
 			} else {
-				vscode.window.setStatusBarMessage("");
+				updateStatusBar("");
 			}
 		}
 	});
