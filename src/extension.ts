2fbce0882c80c9973305be58d49db8104527dee1
Mohamed Alganzory <md.alganzory@gmail.com>
2023-01-05 04:31:45 +0800

    register notes tree view and show in sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import getRegisteredCommands from "./commands";
 import onTabChange from "./handlers/onTabChange";
 import onStart from "./handlers/onStart";
 import onRenameFile from "./handlers/onRenameFile";
-
+import { NotesTreeView } from "./notesTreeView";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,6 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// upon starting VS Code, check if there is a note for the active tab, and display it in the status bar
 	onStart(context);
+
+	// Create the tree view
+	const notesTreeProvider = new NotesTreeView(context);
+	vscode.window.registerTreeDataProvider("notesTreeView", notesTreeProvider);
+
+	vscode.commands.registerCommand("extension.refreshNotes", () =>
+		notesTreeProvider.refresh()
+	);
 
 	// Push all the commands to the context
 	context.subscriptions.push(...getRegisteredCommands(context));
